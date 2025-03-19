7fad4a028e0a35da5f5f1368e9a15534a454cc17
Bartlomiej-json <bartlomiej.serafin.gnom@gmail.com>
2025-03-19 15:30:20 +0000

    2_2 V2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,31 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+        System.out.println("Wpisz trzy numery:");
+        System.out.print("Pierwszy numer: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Drugi numer: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Trzeci numer : ");
+        int num3 = scanner.nextInt();
+
+        int highest;
+
+        if (num1 >= num2 && num1 >= num3) {
+            highest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            highest = num2;
+        } else {
+            highest = num3;
+        }
+
+        System.out.println("Największą liczbą jest : " + highest);
+
+        scanner.close();
+    }
 }
